42bf7ac328fdbdbe7b8e11be1afc3948de2a3a34
agustin_mande <agustinlmande.am@gmail.com>
2024-07-20 19:56:54 -0300

    Realizacion de los Requests

--- app/Http/Requests/EditorialForRequest.php
+++ app/Http/Requests/EditorialForRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LibroForRequest extends FormRequest
+class EditorialForRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,16 +22,9 @@ class LibroForRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'Cod_Libro' => 'required|string|max:10|', 
-            'Titulo' => 'required|string|max:256',
-            'Autor' => 'required|string|max:256',
-            'Editorial' => 'nullable|string|max:256',
-            'Edicion' => 'nullable|string|max:256',
-            'Idioma' => 'nullable|string|max:256',
-            'Estado' => 'required|string|max:256',
+            'Cod_editorial' => 'required|string|max:10', 
+            'NombreEditorial' => 'required|string|max:256',
             'Descripcion' => 'nullable|string',
-            'CantPaginas' => 'required|integer|min:1',
-            'CopiasDisp' => 'required|integer|min:0',
         ];
     }
 }
